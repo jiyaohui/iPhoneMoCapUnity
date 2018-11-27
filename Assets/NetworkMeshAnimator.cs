a35af4fb241d793138b4c7502526fcb1217863ae
jiyaohui <jiyaohui@gmail.com>
2018-11-27 17:36:27 +0800

    copied from JackMac

--- Assets/NetworkMeshAnimator.cs
+++ Assets/NetworkMeshAnimator.cs
@@ -52,12 +52,13 @@ public class NetworkMeshAnimator {
 		Debug.Log("Started accepting messages");
 
 		meshTarget = GameObject.Find ("BlendShapeTarget").GetComponent<SkinnedMeshRenderer> ();
-        meshTargetHuayi = GameObject.Find("ast_huayi_head_1").GetComponent<SkinnedMeshRenderer>();
 
-        if (meshTarget == null) {
+		if (meshTarget == null) {
 			Debug.LogError ("Cannot find BlendShapeTarget. Have you added it to your scene?");
 			return;
         }
+
+        meshTargetHuayi = GameObject.Find("ast_huayi_head_1").GetComponent<SkinnedMeshRenderer>();
 
         if (meshTargetHuayi == null)
         {
@@ -78,37 +79,46 @@ public class NetworkMeshAnimator {
 	public void StopAcceptingMessages() {
 		Debug.Log("Stoped accepting messages");
 		isAcceptingMessages = false;
-	}
+    }
 
-	public bool IsAcceptingMessages() {
-		return isAcceptingMessages;
-	}
+    public bool IsAcceptingMessages()
+    {
+        return isAcceptingMessages;
+    }
 
-	public IEnumerator SetBlendShapesOnMainThread(string messageString) {
+    public IEnumerator SetBlendShapesOnMainThread(string messageString)
+    {
+
+        //var id = 0;
 
         // 对消息字符串中以“|”分隔的每一段进行操作
-		foreach (string message in messageString.Split (new Char[] { '|' }))
-		{
+        foreach (string message in messageString.Split(new Char[] { '|' }))
+        {
+            //id++;
+            //Debug.Log(message + "(" + id + ")");
+
             // 格式化接收到的消息字符串，去除空格和“msg:”字样，便于对应BlendShapes名称
-            var cleanString = message.Replace (" ", "").Replace ("msg:", "");
+            var cleanString = message.Replace(" ", "").Replace("msg:", "");
             // 将消息字符串按分隔符“-”分拆到字符串数组（键值、权重）
-			var strArray  = cleanString.Split (new Char[] {'-'});
+            var strArray = cleanString.Split(new Char[] { '-' });
 
             // 如果收到的是2个字符串（键值：权重）
-			if (strArray.Length == 2) {
+            if (strArray.Length == 2)
+            {
                 // 数组中第2项为权重（浮点型）
-				var weight = float.Parse (strArray.GetValue (1).ToString());
+                var weight = float.Parse(strArray.GetValue(1).ToString());
 
                 // 数组中第2项为BlendShape名称（字符串），且将“_L”和“_R”替换为“Left”和“Right”
-                var mappedShapeName = strArray.GetValue (0).ToString ().Replace ("_L", "Left").Replace ("_R", "Right");
+                var mappedShapeName = strArray.GetValue(0).ToString().Replace("_L", "Left").Replace("_R", "Right");
 
                 // 取得meshTarget（场景中的“BlendShapeTarget”）中该BlendShape名称对应的序号
-                var index = meshTarget.sharedMesh.GetBlendShapeIndex (mappedShapeName);
-                var indexHuayi = meshTargetHuayi.sharedMesh.GetBlendShapeIndex("ARFaceAnchor." +mappedShapeName);
+                var index = meshTarget.sharedMesh.GetBlendShapeIndex(mappedShapeName);
+                var indexHuayi = meshTargetHuayi.sharedMesh.GetBlendShapeIndex(mappedShapeName);
 
                 // 若能找到对应的序号，则将对应的BlendShape设为该序号（名称）对应的权重，否则忽略
-                if (index > -1) {
-					meshTarget.SetBlendShapeWeight (index, weight);
+                if (index > -1)
+                {
+                    meshTarget.SetBlendShapeWeight(index, weight);
                 }
 
                 // 若能找到对应的序号，则将对应的BlendShape设为该序号（名称）对应的权重，否则忽略
@@ -117,9 +127,10 @@ public class NetworkMeshAnimator {
                     meshTargetHuayi.SetBlendShapeWeight(indexHuayi, weight);
                 }
             }
-		}
+        }
 
-		yield return null;
-	}
+        yield return null;
+    }
+
 }
 
